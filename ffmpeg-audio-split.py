4f3e299c8f12b1d7a9cdde0d03411e72fca2bc6f
Maxime Daniel <root@maxux.net>
2023-01-22 06:55:48 +0100

    initial release with ffmpeg-audio-split

---
+++ ffmpeg-audio-split.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import sys
+
+if len(sys.argv) < 5:
+    print("Usage: %s <source-file> <destination-file> <source-time> <length>" % sys.argv[0])
+    sys.exit(1)
+
+timefrom = sys.argv[3]
+timeto = sys.argv[2]
+
+dtfrom = datetime.strptime(timefrom, '%H:%M:%S')
+dtto = datetime.strptime(timeto, '%H:%M:%S')
+delta = dtto - dtfrom
+
+source = sys.argv[1]
+target = sys.argv[2]
+
+cmdline = f"ffmpeg -i {source} -t {delta} -ss {timefrom} -vn -acodec copy -map 0:1 {target}"
+print(cmdline)
+
